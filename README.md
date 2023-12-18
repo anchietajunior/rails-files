00cf0c1ae7fa8a5d71478f5dadc79eb521b93659
José Anchieta <santosjr87@gmail.com>
2023-12-17 23:29:11 -0300

    Add first files

---
+++ README.md
@@ -0,0 +1,23 @@
+# Rails Files
+
+Quick files to add in Rails projects.
+
+## Concerns
+
+- Authenticate
+
+## Models
+
+- AppSession
+- User
+- Current
+
+## Controllers
+
+- AppSessionsController
+- UsersControllers
+
+## Views
+
+- Login Form with Tailwindcss
+- SignUp Form with Tailwindcss
---
+++ models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email,
+    format: { with: URI::MailTo::EMAIL_REGEXP },
+    uniqueness: { case_sensitive: false }
+
+  has_many :app_sessions
+
+  before_validation :strip_extraneous_spaces
+
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, confirmation: true
+
+  def self.create_app_session(email:, password:)
+    return nil unless user = User.find_by(email: email.downcase)
+
+    user.app_sessions.create if user.authenticate(password)
+  end
+  
+  def authenticate_app_session(app_session_id, token)
+    app_sessions.find(app_session_id).authenticate_token(token)
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
+
+  private
+
+  def strip_extraneous_spaces
+    self.name = name&.strip
+    self.email = email&.strip
+  end
+end
